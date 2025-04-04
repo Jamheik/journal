b954399f82c93d9c4a803c8cd9d8d494ce0b38d6
Jamheik <81734621+Jamheik@users.noreply.github.com>
2025-04-04 16:53:41 +0300

    init landing

---
+++ src/components/CalendarComponent.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { View, StyleSheet, SafeAreaView } from "react-native";
+import { Calendar } from "react-native-calendars";
+
+const LandingComponent = () => {
+  const onCalendarPress = (day) =>{
+    alert(JSON.stringify(day.dateString));
+  }
+  return (
+    <SafeAreaView style={styles.container}>
+      <Calendar
+        onDayPress={onCalendarPress}
+        theme={{
+          backgroundColor: '#000000',
+          calendarBackground: '#000000',
+          textSectionTitleColor: '#FFFFFF',
+          selectedDayBackgroundColor: '#1e90ff',
+          selectedDayTextColor: '#FFFFFF',
+          todayTextColor: '#FFFFFF',
+          dayTextColor: '#FFFFFF',
+          textDisabledColor: '#555555',
+        }}
+      />
+    </SafeAreaView>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5f5f5",
+    width: "100%",
+  },
+});
+
+export default LandingComponent;
