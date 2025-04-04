70ce1bf4bc2d86a709993d1912afabef0f52c4b0
Jamheik <heikkinenjami03@gmail.com>
2025-04-04 22:35:11 +0300

    added flatlist to render notes

--- src/components/CalendarComponent.js
+++ src/components/CalendarComponent.js
@@ -2,38 +2,49 @@ import React from "react";
 import { View, StyleSheet, SafeAreaView } from "react-native";
 import { Calendar } from "react-native-calendars";
 
-const LandingComponent = () => {
-  const onCalendarPress = (day) =>{
-    alert(JSON.stringify(day.dateString));
-  }
+const CalendarComponent = () => {
+  const onCalendarPress = (day) => {
+    alert(day.dateString);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <Calendar
         onDayPress={onCalendarPress}
         theme={{
-          backgroundColor: '#000000',
-          calendarBackground: '#000000',
-          textSectionTitleColor: '#FFFFFF',
-          selectedDayBackgroundColor: '#1e90ff',
-          selectedDayTextColor: '#FFFFFF',
-          todayTextColor: '#FFFFFF',
-          dayTextColor: '#FFFFFF',
-          textDisabledColor: '#555555',
+          backgroundColor: "#000000",
+          calendarBackground: "#000000",
+          textSectionTitleColor: "#FFFFFF",
+          selectedDayBackgroundColor: "#1e90ff",
+          selectedDayTextColor: "#FFFFFF",
+          todayTextColor: "#FFFFFF",
+          dayTextColor: "#FFFFFF",
+          textDisabledColor: "#555555",
         }}
+        style={{
+          borderWidth: 1,
+          borderColor: "#FFFFFF",
+          borderRadius: 10,
+          margin: 10,
+          width: "90%",
+          alignSelf: "center",
+        }}
+        hideExtraDays={true}
+        enableSwipeMonths={true}
+        firstDay={1}
       />
     </SafeAreaView>
   );
 };
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
+    alignItems: "stretch",
     backgroundColor: "#f5f5f5",
     width: "100%",
   },
 });
 
-export default LandingComponent;
+export default CalendarComponent;
--- src/views/LandingView.js
+++ src/views/LandingView.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import CalendarComponent from '../components/CalendarComponent';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import CalendarComponent from "../components/CalendarComponent";
+import DailyNoteComponent from "../components/dailyNoteComponent";
 
 const LandingView = () => {
-    return (
-        <View style={styles.container}>
-            <CalendarComponent/>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <CalendarComponent />
+      <DailyNoteComponent />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#f5f5f5',
-    },
-    text: {
-        fontSize: 18,
-        color: '#333',
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "stretch",
+    backgroundColor: "#f5f5f5",
+    width: "100%",
+  },
+
+  text: {
+    fontSize: 18,
+    color: "#333",
+  },
 });
 
 export default LandingView;
